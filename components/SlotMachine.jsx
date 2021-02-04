fce035c10ecbbfc897b0557788ca8b4ce9f2c08d
mbknu <bouchelot.m@gmail.com>
2021-02-04 18:06:27 +0100

    add condition, need rework

--- components/SlotMachine.jsx
+++ components/SlotMachine.jsx
@@ -15,41 +15,100 @@ const SlotMachine = () => {
   };
 
   console.log(result);
-  if (result < 5) {
+  if (result <= 1) {
     const newA = "🥝";
     const newB = "🥝";
     const newC = "🥝";
     return (
       <>
-        <button onClick={() => doTheMath(30)}>Win, play again</button>
+        <h1>WIN</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
         {newA} {newB} {newC}
       </>
     );
-  }
-
-  const generateRandomSlot = () => {};
-
-  if (
-    possibleSlots.a === possibleSlots.b &&
-    possibleSlots.a === possibleSlots.c
-  ) {
+  } else if (result > 1 && result <= 5) {
+    const newA = "🍒";
+    const newB = "🍊";
+    const newC = "🍒";
     return (
       <>
-        <button onClick={() => doTheMath(30)}>Try</button>
-        <h1>
-          {possibleSlots.a} {possibleSlots.b} {possibleSlots.c}{" "}
-        </h1>
-        <p>WIN</p>
+        <h1>LOSE</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
+      </>
+    );
+  } else if (result >= 6 && result <= 10) {
+    const newA = "🥝";
+    const newB = "🥝";
+    const newC = "🍒";
+    return (
+      <>
+        <h1>LOSE</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
+      </>
+    );
+  } else if (result >= 11 && result <= 15) {
+    const newA = "🍒";
+    const newB = "🍊";
+    const newC = "🥝";
+    return (
+      <>
+        <h1>LOSE</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
+      </>
+    );
+  } else if (result >= 11 && result <= 15) {
+    const newA = "🍊";
+    const newB = "🍒";
+    const newC = "🥝";
+    return (
+      <>
+        <h1>LOSE</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
+      </>
+    );
+  } else if (result >= 16 && result <= 20) {
+    const newA = "🍒";
+    const newB = "🍊";
+    const newC = "🥝";
+    return (
+      <>
+        <h1>LOSE</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
+      </>
+    );
+  } else if (result >= 21 && result <= 28) {
+    const newA = "🥝";
+    const newB = "🍒";
+    const newC = "🍒";
+    return (
+      <>
+        <h1>LOSE</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
       </>
     );
   } else {
+    const newA = "🍒";
+    const newB = "🍒";
+    const newC = "🍒";
     return (
       <>
-        <button onClick={() => doTheMath(30)}>Try</button>
-        <h1>
-          {possibleSlots.a} {possibleSlots.b} {possibleSlots.c}{" "}
-        </h1>
-        <p>TRY AGAIN</p>
+        <h1>WIN</h1>
+        <button onClick={() => doTheMath(30)}>Play again</button>
+        <br />
+        {newA} {newB} {newC}
       </>
     );
   }
